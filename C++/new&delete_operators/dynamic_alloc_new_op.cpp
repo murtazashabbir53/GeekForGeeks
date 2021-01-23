689556a613579e83ac241cdb62c44ad4c005d7f8
Murtaza Shabbir <60929735+murtazashabbir53@users.noreply.github.com>
2021-01-23 13:17:35 +0530

    Update and rename C++/new_operator/dynamic_alloc_new_op.cpp to C++/new&delete_operators/dynamic_alloc_new_op.cpp

--- C++/new&delete_operators/dynamic_alloc_new_op.cpp
+++ C++/new&delete_operators/dynamic_alloc_new_op.cpp
@@ -30,3 +30,20 @@ int main(int argc, char **argv)
     return 0;
 }
                                                                        //Memory excution using new operator
+//------------------------------------------------------------------------------------------------------------------------
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int *pointerVal = NULL; //integer value initialized to NULL
+    pointerVal = new int;
+
+    (*pointerVal) = 366;     // given a value to the pointer
+    cout << "Integer pointer value: " << *pointerVal << endl;
+    
+    delete pointerVal;    //delete memory allocated... bcz if not deleted by the programmer itself then memory leak issues will be raised. Anyways it won't be deleted by the compiler
+    return 0;
+}
